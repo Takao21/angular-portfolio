d70f447c6a5054bc0561db0a3578f838c99af1a0
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-04-28 13:06:59 +0630

    added router guards on admin panel

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -6,6 +6,7 @@ import { ProjectCreateComponent } from './project-create/project-create.componen
 import { ProjectUpdateComponent } from './project-update/project-update.component';
 import { ProjectComponent } from './project/project.component';
 import { LoginComponent } from './login/login.component';
+import { AdminGuard } from './admin.guard';
 
 const routes: Routes = [
   {
@@ -14,19 +15,22 @@ const routes: Routes = [
     children: [
       {
         path: 'list',
-        component: ProjectListComponent
+        component: ProjectListComponent,
+        canActivate: [AdminGuard]
       },
       {
         path: 'create',
-        component: ProjectCreateComponent
+        component: ProjectCreateComponent,
+        canActivate: [AdminGuard]
       },
       {
         path: 'update',
-        component: ProjectUpdateComponent
+        component: ProjectUpdateComponent,
+        canActivate: [AdminGuard]
       },
       {
         path: 'login',
-        component: LoginComponent
+        component: LoginComponent,
       },
     ]
   }
